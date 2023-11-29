6b65371d44676b5dae272e4d1fff3f68286039d7
Yem Karki <yemkarki1998@gmail.com>
2023-11-29 14:22:07 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors")({ origin: true });
 const app = express();
-const port = 3000; 
+const port = 8000; 
 
 const MID = "BCRUISEPL";
 const SEC_ID = "bcruisepl";
